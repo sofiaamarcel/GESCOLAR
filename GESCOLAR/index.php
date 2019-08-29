15155f709f4c8dc7de9be4111264db44fdf9dd78
Aluno <aluno@etec.sp.gov.br>
2019-08-29 15:47:40 -0300

    index

--- GESCOLAR/index.php
+++ GESCOLAR/index.php
@@ -1,1 +1,17 @@
-sadfsdfew
+<!DOCTYPE html>
+<html>
+      <head>
+            <link href="css/estilo.css" type="text/css" rel="stylesheet" />
+</head>
+      <body>
+            <div id="global">
+                 <h1>GESCOLAR</h1>
+                 <nav>
+                     <ul>
+                         <li><a href="cadastro_alunos.php">Cadastrar Aluno</a></li>
+                         <li><a href="lista_alunos.php">Lista de Alunos</a></li>
+</ul>
+</nav>
+</div>
+</body>
+</html>
