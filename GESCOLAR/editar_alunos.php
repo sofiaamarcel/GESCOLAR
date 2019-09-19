74d514d761de97652fc3901ab7b4aba11b9e06a3
Aluno <aluno@etec.sp.gov.br>
2019-09-19 16:16:55 -0300

    Modificações

--- GESCOLAR/editar_alunos.php
+++ GESCOLAR/editar_alunos.php
@@ -2,12 +2,10 @@
 /**
  * Arquivo para registrar os dados de um aluno no banco de dados.
  */
-if(isset($_REQUEST['atualizar']))
-{
-     try
-     {
+try {
+    if(isset($_REQUEST['atualizar']))
+    {
          include 'includes/conexao.php';
-
          $sql = "UPDATE alunos SET nome = ?, data_nascimento = ?, sexo =?,
                                    genero = ?, cpf = ?, cidade = ?, estado = ?,
                                    bairro = ?, rua = ?, cep = ?
@@ -27,23 +25,56 @@ if(isset($_REQUEST['atualizar']))
         $stmt->bindParam(11, $_REQUEST['id_aluno']);   
         $stmt->execute();
         
-     } catch(Exception $e) {
-           echo $e->getMessage();
+     } 
+
+    if(isset($_REQUEST['excluir']))
+     {
+         $stmt = $conexão->prepare("DELETE FROM aluno WHERE id = ?");
+         $stmt->bindParam(1, $_REQUEST['id_aluno']);
+         $stmt->execute();
+         header("location: lista_alunos.php");
      }
+
+     $stmt = $conexao->prepare("SELECT * FROM aluno WHERE id = ?");
+     $stmt->bindParam(1, $_REQUEST['id_aluno']);
+     $stmt->execute();
+     $aluno = $stmt->fechObject();
+
+} catch(Exception $e) {
+    echo $e->getMessage();
 }
+
 ?>
 <link href="css/estilos.css" type="text/css" rel="stylesheet" />
+<?php include_once 'includes/cabecalho.php' ?>
 <div>
 <fieldset>
     <legend>Cadastro de Aluno </legend>
         <form action="editar_alunos.php?atualizar=true">
-           <label>Nome: <input type="text" name="nome" required /> </label>
-           <label>Cidade: <input type="text" name="cidade" required /> </label>
-           <label>CEP: <input type="text" name="cep" required /> </label>
-           <label>Bairro: <input type="text" name="bairro" required /> </label>
-           <label>Rua: <input type="text" name="rua" required /> </label>
-           <label>Estado: <input type="text" name="estado" required /> </label>
-           <label>Data Nasc: <input type="text" name="data_nascimento" required /> </label>
+           <label>Nome: 
+               <input type="text" name="nome" required value="<?= $aluno->nome ?>" /> 
+           </label>
+           <label>Cidade:
+               <input type="text" name="cidade" required value="<?= $aluno->cidade ?>" /> 
+           </label>
+           <label>CEP:
+               <input type="text" name="cep" required value="<?= $aluno->cep ?>" /> 
+            </label>
+           <label>Bairro: 
+               <input type="text" name="bairro" required value="<?= $aluno->bairro ?>" /> 
+            </label>
+           <label>Rua: 
+               <input type="text" name="rua" required value="<?= $aluno->rua ?>" /> 
+            </label>
+           <label>Estado: 
+               <input type="text" name="estado" required value="<?= $aluno->estado ?>" /> 
+            </label>
+           <label>Data Nasc: 
+               <input type="text" name="data_nascimento" required value="<?= $aluno->data_nascimento ?>" /> 
+            </label>
+
+            <a href="editar_alunos.php?excluir=true&id=<?= $aluno->id ?>">Excluir</a>
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                 
            <button type="submit">Salvar</button>
     </form>
 </legend>
