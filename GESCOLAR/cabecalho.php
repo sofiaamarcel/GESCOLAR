790b1eb0a923dacb0503a775ce99754ca3f01b38
Aluno <aluno@etec.sp.gov.br>
2019-09-19 16:30:58 -0300

    Cabeçalho

---
+++ GESCOLAR/cabecalho.php
@@ -0,0 +1,10 @@
+<header>
+   <ul id="menu-navegacao">
+      <li> <a href="cadastro_alunos.php">Novo Aluno</a> </li>
+      <li> <a href="lista_alunos.php">Lista de Alunos</a> </li>
+
+      <li> <a href="cadastro_alunos.php">Novo Aluno</a> </li>
+      <li> <a href="lista_alunos.php">Lista de Alunos</a> </li>
+    </ul>
+</header>
+      
